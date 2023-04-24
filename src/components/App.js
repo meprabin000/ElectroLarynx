57b6215d3033b8aef83d0c6dbaed51053688a2a5
Noor <60522271+nooralyasiri@users.noreply.github.com>
2023-04-24 11:08:01 -0500

    Update App.js
    comment out lines that are making it not work for the time being

--- src/components/App.js
+++ src/components/App.js
@@ -14,23 +14,24 @@ import TextToSpeech from './TextToSpeech'
 import AppStyles from '../styles/AppStyles';
 import {useAuth0, Auth0Provider} from 'react-native-auth0';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+//import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const Stack = createNativeStackNavigator();
+//const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Signup" component={Signup} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    //<NavigationContainer>
+    //  <Stack.Navigator screenOptions={{ headerShown: false }}>
+     //   <Stack.Screen name="Login" component={Login} />
+    //    <Stack.Screen name="Signup" component={Signup} />
+    //  </Stack.Navigator>
+   // </NavigationContainer>
     // <Auth0Provider domain={"dev-ejp81yk157h52u8t.us.auth0.com"} clientId={"BdBMWwTTB3MqF1MiPuoAgdkzrpDVd4QM"}>
-    //   <View style={AppStyles.appView}>
-    //     <Login />
-    //   </View>
-    // </Auth0Provider>
+    //</Auth0Provider>
+       <View style={AppStyles.appView}>
+         <Login />
+       </View>
+    
   );
 }
 
